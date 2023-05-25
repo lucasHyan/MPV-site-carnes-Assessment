eff565f83939f2a61d2e251ed514ca78f69e16a8
lucasHyan <lucas.hyan@hotmail.com>
2023-05-25 01:58:20 -0300

    adicionei var no css+radio button custom

--- script.js
+++ script.js
@@ -1,21 +1,34 @@
 // botão voltar ao topo
 window.onscroll = () => {
-
-  if (document.body.scrollTop > 200 || document.documentElement.scrollTop > 200) {
+  if (
+    document.body.scrollTop > 200 ||
+    document.documentElement.scrollTop > 200
+  ) {
     document.getElementById("myBtn").style.display = "block";
   } else {
     document.getElementById("myBtn").style.display = "none";
   }
+};
+function topFunction() {
+  document.documentElement.scrollTop = 0;
+}
 
-  };
-  function topFunction() {
-    document.documentElement.scrollTop = 0; 
-  }
+// radio button
 
+const radioButtons = Array.from(document.body.getElementsByClassName("radio"));
 
+radioButtons.forEach((radio) => {
+  radio.addEventListener("click", () => {
+    const confereRadio = radio.classList.contains("radio-marcado");
 
+    if (confereRadio) {
+      return;
+    }
 
-  // radio button
-  
+    radioButtons.forEach((outroRadio) => {
+      outroRadio.classList.remove("radio-marcado");
+    });
 
-  
+    radio.classList.add("radio-marcado");
+  });
+});
