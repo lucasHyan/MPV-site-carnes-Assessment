25976813fdcb2b6b41df8e2f09cd00534548c04f
lucasHyan <lucas.hyan@hotmail.com>
2023-06-16 20:20:29 -0300

    formulario pronto

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-"use strict"
+"use strict";
 
 // botão voltar ao topo
 window.onscroll = () => {
@@ -38,7 +38,7 @@ radioButtons.forEach((element) => {
 //select
 const selectOptions = document.querySelector(".options");
 const selectBtn = document.querySelector(".select-btn");
-const selectBtnSpan = document.querySelector(".select-btn span");
+let selectBtnSpan = document.querySelector(".select-btn span");
 const selectOption = document.querySelectorAll(".option");
 const arrowIcon = document.querySelector(".arrow-icon");
 
@@ -47,7 +47,7 @@ selectBtn.addEventListener("click", () => {
   arrowIcon.classList.toggle("rotate");
 });
 
-selectOption.forEach(element => {
+selectOption.forEach((element) => {
   element.addEventListener("click", () => {
     selectBtnSpan.innerText = element.innerText;
     selectOptions.classList.toggle("desativa");
@@ -55,22 +55,26 @@ selectOption.forEach(element => {
   });
 });
 
-
 //checkbox
 const containerCheckbox = document.querySelector(".containerCheckbox");
 const checkboxElemento = document.querySelectorAll(".checkbox");
-const checkboxAmbos =  document.querySelector(".checkbox-ambos");
+const checkboxAmbos = document.querySelector(".checkbox-ambos");
 
-checkboxElemento.forEach(elemento => {
+checkboxElemento.forEach((elemento) => {
   elemento.addEventListener("click", () => {
     elemento.classList.toggle("checkbox-marcado");
 
-    const elemento1IsChecked = checkboxElemento[0].classList.contains("checkbox-marcado");
-    const elemento2IsChecked = checkboxElemento[1].classList.contains("checkbox-marcado");
+    const elemento1IsChecked =
+      checkboxElemento[0].classList.contains("checkbox-marcado");
+    const elemento2IsChecked =
+      checkboxElemento[1].classList.contains("checkbox-marcado");
 
-    if (elemento1IsChecked && !elemento2IsChecked || !elemento1IsChecked && elemento2IsChecked) {
+    if (
+      (elemento1IsChecked && !elemento2IsChecked) ||
+      (!elemento1IsChecked && elemento2IsChecked)
+    ) {
       checkboxAmbos.classList.add("checkbox-horizontal-marcado");
-    } else if (elemento1IsChecked && elemento2IsChecked) { 
+    } else if (elemento1IsChecked && elemento2IsChecked) {
       checkboxAmbos.classList.remove("checkbox-horizontal-marcado");
       checkboxAmbos.classList.add("checkbox-marcado");
     } else {
@@ -86,33 +90,30 @@ checkboxAmbos.addEventListener("click", () => {
   checkboxAmbos.classList.remove("checkbox-horizontal-marcado");
 
   if (checkboxAmbos.classList.contains("checkbox-marcado")) {
-    checkboxElemento.forEach(elemento => {
+    checkboxElemento.forEach((elemento) => {
       elemento.classList.add("checkbox-marcado");
     });
   } else {
-    checkboxElemento.forEach(elemento => {
+    checkboxElemento.forEach((elemento) => {
       elemento.classList.remove("checkbox-marcado");
     });
   }
 });
 
-
 //form
 
-const form = document.querySelector('form');
+const form = document.querySelector("form");
 const nomeInput = document.querySelector('input[name="name"]');
 const emailInput = document.querySelector('input[name="email"]');
 const telefoneInput = document.querySelector('input[name="telefone"]');
 const mensagemInput = document.querySelector('textarea[name="message"]');
 
-form.addEventListener('submit', (event) => {
+form.addEventListener("submit", (event) => {
   event.preventDefault();
-  
 
-
-// Validar nome
+  // Validar nome
   const nomeTrim = nomeInput.value.trim();
-  if (nomeTrim === '' || nomeTrim.split(' ').length < 2) {
+  if (nomeTrim === "" || nomeTrim.split(" ").length < 2) {
     alert(`Nome não pode estar em branco e deve possuir pelo menos 2 nomes`);
     return;
   }
@@ -120,38 +121,54 @@ form.addEventListener('submit', (event) => {
   // Validar email
   const email = emailInput.value.trim();
   if (!/^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(email)) {
-    alert('E-mail inválido');
+    alert("E-mail inválido");
     return;
   }
 
   // Validar telefone
-  const telefone = telefoneInput.value.replace(/[\(\)\-\s]/g, '');
+  const telefone = telefoneInput.value.replace(/[\(\)\-\s]/g, "");
   if (telefone.length !== 11) {
-    alert('Telefone inválido');
+    alert("Telefone inválido");
     return;
   }
-  
+
   // Validar mensagem
   const mensagem = mensagemInput.value.trim();
   if (mensagem.length < 5) {
-    alert('Por favor escreva uma mensagem com pelo menos 5 caracteres');
+    alert("Por favor escreva uma mensagem com pelo menos 5 caracteres");
     return;
   }
 
+  // Validar radio buttons
+  const radioMarcadoBoolean = radioButtons.some((element) =>
+    element.classList.contains("radio-marcado")
+  );
 
-// Validar radio buttons
-const radioMarcadoBoolean = radioButtons.some(element =>
-   element.classList.contains('radio-marcado'));
+  if (!radioMarcadoBoolean) {
+    alert("Selecione uma preferência de carnes");
+    return;
+  }
 
-if (!radioMarcadoBoolean) {
-  alert('Selecione uma preferência de carnes');
-  return;
-}
+  //validar select
+  if (selectBtnSpan.innerText === "Selecione uma opção") {
+    alert("Selecione uma opção de tipos de carnes");
+    return;
+  }
 
-//validar select
+  //validar checkbox
+  const checkboxComAmbos = document.querySelectorAll(
+    ".checkbox, .checkbox-ambos"
+  );
 
+  const checkboxMarcadoBoolean = Array.from(checkboxComAmbos).some((element) =>
+    element.classList.contains("checkbox-marcado")
+  );
 
+  if (!checkboxMarcadoBoolean) {
+    alert("Selecione uma opção de contato");
+    return;
+  }
 
-// Se chegou aqui, o formulário está válido
-alert('Formulário enviado com sucesso!');
+  // Se chegou aqui, o formulário está válido
+  alert("Formulário enviado com sucesso!");
 });
